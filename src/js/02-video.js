0a05ecfa72b7692f3d38be01a61b8bb1e48068f6
Andrii-Semeniv <andrii.semeniv7@gmail.com>
2023-02-07 02:18:36 +0200

    TaskVideoplayerDone

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,26 @@
+import throttle from 'lodash.throttle';
+import Player from '@vimeo/player';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+const currentTime = ({ seconds }) => {
+  localStorage.setItem('videoplayer-current-time', JSON.stringify(seconds));
+};
+
+player.on('timeupdate', throttle(currentTime, 1000));
+
+const pauseTime = localStorage.getItem('videoplayer-current-time');
+
+player
+  .setCurrentTime(pauseTime)
+  .then(function (seconds) {})
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        break;
+
+      default:
+        break;
+    }
+  });
